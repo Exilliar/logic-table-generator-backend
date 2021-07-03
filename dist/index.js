a2efb871fd765d1f2162ac61fc13dfd31fd39e0c
exilliar <rorywebber1000@gmail.com>
2021-07-03 20:27:57 +0100

    #3 made url, all works

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const tree_1 = require("./tree");
 const app = express_1.default();
 const port = 8080; // default port to listen
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
+});
+app.get("/results/:expression", (req, res) => {
+    const controller = new tree_1.TreeController(req.params.expression);
+    const results = controller.calcResults();
+    res.send(results);
 });
 // start the Express server
 app.listen(port, () => {
